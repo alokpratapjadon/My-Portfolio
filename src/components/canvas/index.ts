cbffb6a6015e91e1aa8b07a0a9f8cb393775a244
ALOK PRATAP JADON <alok8273739734@gmail.com>
2025-06-20 11:50:40 +0530

    commit

--- src/components/canvas/index.ts
+++ src/components/canvas/index.ts
@@ -1,6 +1,6 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import ComputersCanvas from "./Computers";
-import StarsCanvas from "./Stars";
+import EarthCanvas from "./earth";
+import BallCanvas from "./ball";
+import ComputersCanvas from "./computers";
+import StarsCanvas from "./stars";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas };
